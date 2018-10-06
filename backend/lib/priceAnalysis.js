3ddbbc99792357bf36dbd9f6090fae7bc1256ba4
Kenya Barnes <Barneskenya@gmail.com>
2018-10-06 16:12:19 -0400

    added priceAnalysis.js and updated index.js

---
+++ backend/lib/priceAnalysis.js
@@ -0,0 +1,7 @@
+'use strict'
+let output = {
+    price: 32
+};
+module.exports = async function (data){
+    return output;
+}
--- backend/plugins/slash/index.js
+++ backend/plugins/slash/index.js
@@ -1,13 +1,22 @@
 'use strict';
 
 module.exports = function (fastify, opts, next) {
-  fastify.get('/', (req, reply) => {
+  fastify.get('/', (req, reply) =>  {
         reply.view('/pages/slash/index', {})
     });
     fastify.post('/:data', (req, reply) => {
         let payload = JSON.parse(req.params.data);
-
-        reply.send(payload.itemName + 'helloworld');
+        const path = require('path');
+        const priceAnalysis = require(path.resolve('lib/priceAnalysis.js'));
+        priceAnalysis(payload)
+          .then((data) => {
+            payload.price = data.price;
+            reply.send(payload.price);
+          })
+          .catch((err) => {
+            if (err) throw err;
+          })
+        
     });
 
   next()
