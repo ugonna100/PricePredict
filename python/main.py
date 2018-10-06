34d64f4b97329212aa7106de2244580ef8626dbe
LAPTOP-7N17U8PH\Bakuano <ugonna100@gmail.com>
2018-10-06 14:42:07 -0400

    Flask API now calls temporary name for ML alg

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import subprocess
+import regressor
 
 app = Flask(__name__)
 CORS(app)
@@ -8,5 +9,14 @@ CORS(app)
 
 @app.route('/', methods=["POST", "OPTION"])
 def decision():
-    html = "<h3>Hello you're here</h3>"
     content = request.json
+    if content['desc'] != "":
+        resultPrice = pricePrediction(content['desc'])
+        resultSend = {'price': resultPrice}
+        return jsonify(resultSend)
+    else:
+        resultSend = {'price': -1}
+        return jsonify(resultSend)
+
+def pricePrediction(description):
+    return regressor.input(description)
