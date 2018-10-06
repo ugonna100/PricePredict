c16e5611811626ad9e4e50943a810de7e28f0c7e
KeAndre96 <keandregraham@hotmail.com>
2018-10-06 14:48:20 -0400

    Changed font size and color

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+    return <h1> <p><a> Price Predict {props.name} </a></p> </h1>;
   }
   
-  const element = <Welcome name="Sara" />;
+  const element = <Welcome name="PricePredict.com" />;
   ReactDOM.render(
     element,
     document.getElementById('root')
