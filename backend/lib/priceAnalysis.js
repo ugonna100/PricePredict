374993a6b7d717e0ebd83126bb4cfddba4ace97c
Kenya Barnes <Barneskenya@gmail.com>
2018-10-06 23:49:01 -0400

    pulled node server and API handler

--- backend/lib/priceAnalysis.js
+++ backend/lib/priceAnalysis.js
@@ -1,7 +1,20 @@
 'use strict'
-let output = {
-    price: 32
-};
+
+const request = require('request');
+
+function getData(){
+    return new Promise((resolve, reject) => {
+        let options = {
+            method: 'POST', 
+            url: 'http://0.0.0.0:5000'
+        }
+        request(options, (err, response, body) => {
+            if (err) throw err;
+            return resolve(JSON.parse(body));
+        })
+    });
+}
+
 module.exports = async function (data){
-    return output;
+    return getData();
 }
--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import subprocess
-import regressor
+#import regressor
 
 app = Flask(__name__)
 CORS(app)
@@ -9,14 +9,18 @@ CORS(app)
 
 @app.route('/', methods=["POST", "OPTION"])
 def decision():
-    content = request.json
-    if content['desc'] != "":
-        resultPrice = pricePrediction(content['desc'])
-        resultSend = {'price': resultPrice}
-        return jsonify(resultSend)
-    else:
-        resultSend = {'price': -1}
-        return jsonify(resultSend)
+    #content = request.json
+    #print(content)
+    #if content['desc'] is not "":
+        #resultPrice = pricePrediction(content['desc'])
+    resultPrice = 64
+    resultSend = {'price': resultPrice}
+    return jsonify(resultSend)
+    #else:
+    #   resultSend = {'price': -1}
+    #  return jsonify(resultSend)
 
 def pricePrediction(description):
     return regressor.input(description)
+
+app.run('0.0.0.0')
